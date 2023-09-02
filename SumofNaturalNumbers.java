1ab760302d519554b455c24fb35007d2c02f5864
vineethavemulapalli <143867440+vineethavemulapalli@users.noreply.github.com>
2023-09-02 12:18:59 +0530

    Create SumofNaturalNumbers.java

---
+++ SumofNaturalNumbers.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class SumofNaturalNumbers{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(system.in);
+System.out.println("Enter n value in between 1 to 20: ");
+int n=sc.nextInt();
+int sum=0;
+for(int i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.println("Sum of first"+n+"natural numbers is"+sum);
+}
+}
