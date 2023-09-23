fd57106822cb2dbbacac4d2413c4bb160fd3ace7
vineethavemulapalli <143867440+vineethavemulapalli@users.noreply.github.com>
2023-09-23 12:13:35 +0530

    Create PrimeList.java

---
+++ PrimeList.java
@@ -0,0 +1,24 @@
+public class PrimeList {
+
+    public static void main(String[] args) {
+
+        int low = 23, high = 97;
+
+        while (low <=high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && low != 0 && low != 1)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
