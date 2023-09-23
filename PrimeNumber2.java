5106ac63ec107bc55824d301ecc318646b4999d6
vineethavemulapalli <143867440+vineethavemulapalli@users.noreply.github.com>
2023-09-23 11:51:41 +0530

    Create PrimeNumber2.java

---
+++ PrimeNumber2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class PrimeNumber2 {
+    static{
+        System.out.println(
+            "Enter postive integer to check prime number or not: ");
+    }
+  public static void main(String[] args) {
+      int num=Integer.parseInt(args[0]);
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
